16ca51b23757e625f569c3e5dbef7bacc96413cb
petr <py0trandreev@yandex.ru>
2021-12-28 20:55:38 +0300

    show product list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import hello from './module.js'
+import getProductList from "./mock/data.js";
+import renderGoodsList from "./showcase.js";
 
-import _ from 'lodash'
+const productList = getProductList(20);
 
-hello(_.camelCase('super_admin'))
+renderGoodsList(productList);
