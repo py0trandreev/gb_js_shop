162f954ec8a724bef07ba1556333ea17d366c264
petr <py0trandreev@yandex.ru>
2021-12-30 23:53:50 +0300

    lesson 2 is over

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import { send } from './utils.js';
 
-const productList = getProductList(20);
+const API_URL = 'http://localhost:3000/api/v1';
 
-renderGoodsList(productList);
+let productList = [];
+let cart = [];
+
+send((error) => { console.log(err) }, (res) => {
+    let list = JSON.parse(res);
+    productList = list;
+    renderGoodsList(productList);
+}, `${API_URL}/catalog`)
+
+//Пользователь добавляет товар в корзину
+let buyed = { id: 5, title: "new", price: 999 };
+send((error) => { console.log(err) }, (res) => {
+    cart.push(buyed)
+}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), { "content-type": "application/json" })
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,15 @@ module.exports = {
         }),
     ],
 
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+
+        },
+        compress: true,
+        port: 9000
+    },
+
 
     mode: 'development',
 
