5b92731cdbef2d47f674cfac62bcccbd99d10fc5
petr <py0trandreev@yandex.ru>
2022-01-16 11:30:20 +0300

    some corrected

--- src/CartModel.mjs
+++ src/CartModel.mjs
@@ -1,4 +1,4 @@
-import ProductList from './ProductList.mjs'
+import ProductList from './ProductList.mjs';
 
 export default class CartModel extends ProductList {
     constructor(apiHandler, eventEmmiter) {
@@ -43,40 +43,5 @@ export default class CartModel extends ProductList {
             );
         }
     }
-
-
-
-
-    // remove(id) {
-    //     const index = this.list.findIndex((item) => item.id === id);
-
-    //     //проверяем, что index >= 0, если не был найден, то возврашается -1
-    //     if (index >= 0) {
-    //         this.api.removeFromCart(
-    //             () => {
-    //                 //зд. мы создаем новый массив и добавляем 
-    //                 //все элементы   vvv -из list  от  0го до index-индекса (не включая index)
-    //                 this.list = [...this.list.slice(0, index), ...this.list.slice(index + 1)]
-    //                 //после добавляем все оставшиеся элементы     ^^^  из list начиная с index+1 индекса
-    //                 //до конца массива
-
-    //             },
-    //             () => {
-    //                 //onError
-    //             },
-    //             this.list[index]
-    //         )
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
 
 }
