645837b68fa49e5c493e7598bc41ebc7d9c84f6a
petr <py0trandreev@yandex.ru>
2022-01-16 11:00:55 +0300

    tests created

--- spec/cart_model.spec.mjs
+++ spec/cart_model.spec.mjs
@@ -11,3 +11,25 @@ describe('CartModel.fetch', () => {
         expect(model.list.length).toBeGreaterThan(0);
     });
 });
+
+describe('CartModel.add', () => {
+    it('добавление данных', () => {
+        let product = { id: 15, title: "apple", price: 3 };
+        model.add(product);
+        let product_poped = model.list.pop();
+        expect(product_poped.title).toBe("apple");
+        expect(product_poped.price).toBe(3);
+    });
+});
+
+describe('CartModel.remove', () => {
+    it('удаление данных', () => {
+        const product = { id: 25, title: "pineapple", price: 10 };
+        model.add(product);
+        model.remove(product.id);
+
+        //в корзине не должно быть товара с id = 25
+        const isProductInCart = (item) => item.id === product.id;
+        expect(model.list.findIndex(isProductInCart)).toBe(-1);
+    });
+});
--- src/CartModel.mjs
+++ src/CartModel.mjs
@@ -10,7 +10,7 @@ export default class CartModel extends ProductList {
     fetch(onError) {
         this.api.getCart(
             (data) => {
-                this.list = JSON.parse(data)
+                this.list = JSON.parse(data);
                 this.eventEmmiter.emit('cartFeched', this.list);
             },
             onError
@@ -27,16 +27,56 @@ export default class CartModel extends ProductList {
         );
     }
 
+
+
+
     remove(id, onError) {
         if (this.find(id)) {
             this.api.removeFromCart(
                 () => {
-                    this.remove(id);
+                    this.removeById(id);
+                    // this.list = [...this.list.slice(0, index), ...this.list.slice(index + 1)];
+                    // console.log(`index ${index}`);
                 },
                 onError,
-                this.list[index]
+                this.list[this.itemIndex]
             );
         }
     }
+
+
+
+
+    // remove(id) {
+    //     const index = this.list.findIndex((item) => item.id === id);
+
+    //     //проверяем, что index >= 0, если не был найден, то возврашается -1
+    //     if (index >= 0) {
+    //         this.api.removeFromCart(
+    //             () => {
+    //                 //зд. мы создаем новый массив и добавляем 
+    //                 //все элементы   vvv -из list  от  0го до index-индекса (не включая index)
+    //                 this.list = [...this.list.slice(0, index), ...this.list.slice(index + 1)]
+    //                 //после добавляем все оставшиеся элементы     ^^^  из list начиная с index+1 индекса
+    //                 //до конца массива
+
+    //             },
+    //             () => {
+    //                 //onError
+    //             },
+    //             this.list[index]
+    //         )
+    //     }
+    // }
+
+
+
+
+
+
+
+
+
+
 
 }
